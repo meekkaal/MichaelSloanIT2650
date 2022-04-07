a1a3677e4b6a35c43824cfe7eba784078f6ff89a
Michael Sloan <mikesloan47@gmail.com>
2022-04-06 22:11:52 -0400

    initial commit

---
+++ MichaelSloanWeek3Chap3Problems1-3-5/Chapter 3 Homework/Chapter3Problem3/src/chapter3problem3/Chapter3Problem3.java
@@ -0,0 +1,25 @@
+/*
+ * IT-2650 Java Programming, Spring 2022
+ * Instructor: Martin P. Walsh
+ * Student Name: Michael Sloan
+ * Homework Assignment: Chap 3 - Problem 3
+ * Purpose of Assignment:
+ */
+package chapter3problem3;
+
+/**
+ *
+ * @author Michael Sloan
+ */
+public class Chapter3Problem3
+{
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    // TODO code application logic here
+  }
+  
+}
---
+++ MichaelSloanWeek3Chap3Problems1-3-5/Chapter 3 Homework/Chapter3Problem5/src/chapter3problem5/Chapter3Problem5.java
@@ -0,0 +1,25 @@
+/*
+ * IT-2650 Java Programming, Spring 2022
+ * Instructor: Martin P. Walsh
+ * Student Name: Michael Sloan
+ * Homework Assignment: Chap 3 - Problem 5
+ * Purpose of Assignment:
+ */
+package chapter3problem5;
+
+/**
+ *
+ * @author torna
+ */
+public class Chapter3Problem5
+{
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    // TODO code application logic here
+  }
+  
+}
