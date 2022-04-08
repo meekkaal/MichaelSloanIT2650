6521625b740e1bc90f9006e199630503e31e93d6
Michael Sloan <mikesloan47@gmail.com>
2022-04-08 16:35:04 -0400

    Update Chapter3Problem5.java

--- MichaelSloanWeek3Chap3Problems1-3-5/Chapter 3 Homework/Chapter3Problem5/src/chapter3problem5/Chapter3Problem5.java
+++ MichaelSloanWeek3Chap3Problems1-3-5/Chapter 3 Homework/Chapter3Problem5/src/chapter3problem5/Chapter3Problem5.java
@@ -3,13 +3,15 @@
  * Instructor: Martin P. Walsh
  * Student Name: Michael Sloan
  * Homework Assignment: Chap 3 - Problem 5
- * Purpose of Assignment:
+ * Purpose of Assignment: Write a program to compute monthly payment and amortization of a loan.
  */
 package chapter3problem5;
 
+import java.util.Scanner;
+
 /**
  *
- * @author torna
+ * @author Michael Sloan
  */
 public class Chapter3Problem5
 {
@@ -24,6 +26,17 @@ public class Chapter3Problem5
     System.out.println("Homework Assignment: Chapter 2, Problem 5");
     System.out.println("_________________________________");
     System.out.println("");
+    
+    System.out.println("Enter the House Purchase Cost");
+    
+    System.out.println("Enter the Down Payment");
+    
+    System.out.println("Enter the Annual Interest Rate as a whole number");
+    
+    System.out.println("Enter the Term in Months");
+    
+    System.out.println("The Monthly Payment is: ");
+    
   }
   
 }
