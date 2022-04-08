7d76a90aa9e69f2e506434d7bba7783e3e2864df
Michael Sloan <mikesloan47@gmail.com>
2022-04-08 00:45:02 -0400

    finish implementation

--- MichaelSloanWeek3Chap3Problems1-3-5/Chapter 3 Homework/Chapter3Problem1/src/chapter3problem1/Chapter3Problem1.java
+++ MichaelSloanWeek3Chap3Problems1-3-5/Chapter 3 Homework/Chapter3Problem1/src/chapter3problem1/Chapter3Problem1.java
@@ -3,9 +3,11 @@
  * Instructor: Martin P. Walsh
  * Student Name: Michael Sloan
  * Homework Assignment: Chap 3 - Problem 1
- * Purpose of Assignment:
+ * Purpose of Assignment: Write a program to estimate the square root of a given number.
  */
 package chapter3problem1;
+
+import java.util.Scanner;
 
 /**
  *
@@ -24,6 +26,25 @@ public class Chapter3Problem1
     System.out.println("Homework Assignment: Chapter 3, Problem 1");
     System.out.println("_________________________________");
     System.out.println("");
+    
+    Scanner keyboard = new Scanner(System.in);
+    
+    System.out.println("This program estimates square roots.");
+    System.out.println("Enter an integer to estimate the square root of:");
+    int number = keyboard.nextInt();
+    double guess = number / 2.0;
+    double newGuess;
+    
+    do
+    {
+      newGuess = number / guess;
+      guess = (guess + newGuess) / 2.0;
+      System.out.println("Current guess: " + guess);
+    } while ((guess - newGuess) / guess > 0.01 && Math.pow(guess, 2) != number);
+    
+    System.out.printf("The estimated square root of %d is: %6.2f%n", number, guess);
+    
+    
   }
   
 }
