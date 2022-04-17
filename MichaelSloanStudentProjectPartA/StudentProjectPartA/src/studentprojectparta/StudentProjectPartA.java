631fc28fc8a470fc17c6604bbcbf271789050cf8
Michael Sloan <mikesloan47@gmail.com>
2022-04-16 23:45:30 -0400

    Create StudentProjectPartA.java

---
+++ MichaelSloanStudentProjectPartA/StudentProjectPartA/src/studentprojectparta/StudentProjectPartA.java
@@ -0,0 +1,18 @@
+package studentprojectparta;
+
+/**
+ *
+ * @author Michael Sloan
+ */
+public class StudentProjectPartA
+{
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    // TODO code application logic here
+  }
+  
+}
