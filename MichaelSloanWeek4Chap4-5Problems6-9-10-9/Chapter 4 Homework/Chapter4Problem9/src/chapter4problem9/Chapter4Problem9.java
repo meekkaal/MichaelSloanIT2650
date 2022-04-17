fd4739f6b31d34a189e337a1008d1c7a5e869793
Michael Sloan <mikesloan47@gmail.com>
2022-04-16 23:07:49 -0400

    Update Chapter4Problem9.java

--- MichaelSloanWeek4Chap4-5Problems6-9-10-9/Chapter 4 Homework/Chapter4Problem9/src/chapter4problem9/Chapter4Problem9.java
+++ MichaelSloanWeek4Chap4-5Problems6-9-10-9/Chapter 4 Homework/Chapter4Problem9/src/chapter4problem9/Chapter4Problem9.java
@@ -7,7 +7,6 @@
  */
 package chapter4problem9;
 
-
 /**
  *
  * @author Michael Sloan
@@ -25,30 +24,30 @@ public class Chapter4Problem9
     System.out.println("Homework Assignment: Chapter 4, Problem 9");
     System.out.println("_________________________________");
     System.out.println("");
-    
+
     SpeciesRecord Unicorn = new SpeciesRecord();
     Unicorn.setName("Unicorn");
-    
+
     SpeciesRecord Rabbit = new SpeciesRecord("Rabbit");
     SpeciesRecord Horse = new SpeciesRecord("Horse", 3000);
     Horse.setGrowthRate(.567);
-    
+
     SpeciesRecord Dodo = new SpeciesRecord("Dodo", 0, -.335);
     Dodo.setPopulation(2);
-    
+
     SpeciesRecord GermanShepherd = new SpeciesRecord("German Sheperd", 550, .45);
-    
+
     System.out.println("Getting population of rabbits: " + Rabbit.getPopulation());
     System.out.println("Getting growth rate of German Shepherds: " + GermanShepherd.getGrowthRate());
     System.out.println("Getting name from first record: " + Unicorn.getName());
-    
+
     System.out.println("The Dodo is endangered: " + Dodo.isEndangered());
     System.out.println("The rabbit is endangered: " + Rabbit.isEndangered());
     System.out.println("Horses and Unicorns are the same: " + Horse.equals(Unicorn));
-    
+
     System.out.println("Making an identical object of German Shepherds.");
     SpeciesRecord GermanShepherd2 = new SpeciesRecord(GermanShepherd);
     System.out.println("Duplicate is same as original German Shepherd: " + GermanShepherd2.equals(GermanShepherd));
   }
-  
+
 }
