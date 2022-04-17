5a36a3062d1f2447b697b5e12c9397c1e6b5a2ff
Michael Sloan <mikesloan47@gmail.com>
2022-04-16 23:44:57 -0400

    Create StudentProjectPartB.java

---
+++ MichaelSloanStudentProjectPartB/StudentProjectPartB/src/studentprojectpartb/StudentProjectPartB.java
@@ -0,0 +1,18 @@
+package studentprojectpartb;
+
+/**
+ *
+ * @author torna
+ */
+public class StudentProjectPartB
+{
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    // TODO code application logic here
+  }
+  
+}
