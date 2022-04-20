a4a600fecc2cb64d57f978c198986de432811e76
Michael Sloan <mikesloan47@gmail.com>
2022-04-19 21:35:48 -0400

    Create Chapter6Problem6.java

---
+++ MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 6 Homework/Chapter6Problem6/src/chapter6problem6/Chapter6Problem6.java
@@ -0,0 +1,29 @@
+/*
+ * IT-2650 Java Programming, Spring 2022
+ * Instructor: Martin P. Walsh
+ * Student Name: Michael Sloan
+ * Homework Assignment: Chap 6 - Problem 6
+ * Purpose of Assignment:
+ */
+package chapter6problem6;
+
+/**
+ *
+ * @author Michael Sloan
+ */
+public class Chapter6Problem6
+{
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    System.out.println("IT-2650 Java Programming");
+    System.out.println("Student Name: Michael Sloan");
+    System.out.println("Homework Assignment: Chapter 6, Problem 6");
+    System.out.println("_________________________________");
+    System.out.println("");
+  }
+  
+}
