e6d23e9c53d7b2f26ab9e6ee178665a4cea08f60
Michael Sloan <mikesloan47@gmail.com>
2022-04-22 22:07:24 -0400

    Update Chapter6Problem6.java

--- MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 6 Homework/Chapter6Problem6/src/chapter6problem6/Chapter6Problem6.java
+++ MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 6 Homework/Chapter6Problem6/src/chapter6problem6/Chapter6Problem6.java
@@ -7,6 +7,7 @@
  */
 package chapter6problem6;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -26,14 +27,42 @@ public class Chapter6Problem6
     System.out.println("Homework Assignment: Chapter 6, Problem 6");
     System.out.println("_________________________________");
     System.out.println("");
-    
+
     Scanner keyboard = new Scanner(System.in);
-    int[] numberArray = new int[50];
-    
+
     System.out.println("How many numbers to enter? (At most 50)");
     int amount = keyboard.nextInt();
+    int[] numberArray = new int[amount];
     System.out.println("Enter each of the " + amount + " numbers. Please put a space between each number.");
-    
+
+    for (int i = 0; i < numberArray.length; i++)
+    {
+      numberArray[i] = keyboard.nextInt();
+    }
+
+    System.out.printf("%-15s%-15s %n", "N", "Count");
+    Arrays.sort(numberArray);
+    int count = 1;
+
+    for (int i = numberArray.length - 1; i >= 0; i--)
+    {
+      if (i > 0 && numberArray[i] == numberArray[i - 1])
+      {
+        count++;
+      } else
+      {
+        if (count <= 1)
+        {
+          System.out.printf("%-15s%-15s %n", numberArray[i], count);
+        } else
+        {
+          System.out.printf("%-15s%-15s %n", numberArray[i], count);
+          count = 1;
+        }
+
+      }
+    }
+
   }
-  
+
 }
