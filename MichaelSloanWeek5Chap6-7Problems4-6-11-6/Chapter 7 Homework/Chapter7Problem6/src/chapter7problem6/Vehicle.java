5a7fcdc26307ea796522e3d570dcf1d29ba72f24
Michael Sloan <mikesloan47@gmail.com>
2022-04-24 03:42:34 -0400

    Update Vehicle.java

--- MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 7 Homework/Chapter7Problem6/src/chapter7problem6/Vehicle.java
+++ MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 7 Homework/Chapter7Problem6/src/chapter7problem6/Vehicle.java
@@ -6,16 +6,26 @@ package chapter7problem6;
  */
 public class Vehicle
 {
-
   private String manufacturerName;
   private int cylinders;
   private Person owner;
 
+  /**
+   *
+   */
   public Vehicle()
   {
-
+    this.manufacturerName = "None";
+    this.cylinders = 0;
+    this.owner = new Person();
   }
-
+  
+  /**
+   *
+   * @param manufacturerName
+   * @param cylinders
+   * @param owner
+   */
   public Vehicle(String manufacturerName, int cylinders, Person owner)
   {
     this.manufacturerName = manufacturerName;
@@ -23,50 +33,113 @@ public class Vehicle
     this.owner = owner;
   }
 
+  /**
+   *
+   * @param manufacturerName
+   * @param cylinders
+   */
   public Vehicle(String manufacturerName, int cylinders)
   {
     this.manufacturerName = manufacturerName;
     this.cylinders = cylinders;
   }
+  
+  /**
+   *
+   * @param vehicle
+   */
+  public Vehicle(Vehicle vehicle)
+  {
+    manufacturerName = vehicle.manufacturerName;
+    cylinders = vehicle.cylinders;
+    owner = vehicle.owner;
+  }
 
+  /**
+   *
+   * @return
+   */
   public String getManufacturerName()
   {
     return manufacturerName;
   }
 
+  /**
+   *
+   * @param manufacturerName
+   */
   public void setManufacturerName(String manufacturerName)
   {
     this.manufacturerName = manufacturerName;
   }
 
+  /**
+   *
+   * @return
+   */
   public int getCylinders()
   {
     return cylinders;
   }
 
+  /**
+   *
+   * @param cylinders
+   */
   public void setCylinders(int cylinders)
   {
     this.cylinders = cylinders;
   }
 
+  /**
+   *
+   * @return
+   */
   public Person getOwner()
   {
     return owner;
   }
 
+  /**
+   *
+   * @param owner
+   */
   public void setOwner(Person owner)
   {
     this.owner = owner;
   }
 
+  /**
+   *
+   * @return
+   */
   public String toString()
   {
-    return "";
+    return manufacturerName + ", " + cylinders + " cylinders, owned by " + owner;
   }
 
-  public boolean equals(Vehicle other)
+  /**
+   *
+   * @param other
+   * @return
+   */
+  public boolean equals(Object other)
   {
-    return true;
+    if (other == null)
+    {
+      return false;
+    }
+    else if (getClass() != other.getClass())
+    {
+      return false;
+    }
+    else
+    {
+      Vehicle otherVehicle = (Vehicle)other;
+      return (manufacturerName.equals(otherVehicle.manufacturerName) 
+              && cylinders == otherVehicle.cylinders 
+              && owner.equals(otherVehicle.owner));
+    }
   }
 
 }
