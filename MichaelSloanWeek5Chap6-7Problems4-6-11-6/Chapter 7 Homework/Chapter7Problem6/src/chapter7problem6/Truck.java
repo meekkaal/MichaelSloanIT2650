a9bcfed09a30d2cf98d299b05c17775751371735
Michael Sloan <mikesloan47@gmail.com>
2022-04-23 20:12:26 -0400

    Update Truck.java

--- MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 7 Homework/Chapter7Problem6/src/chapter7problem6/Truck.java
+++ MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 7 Homework/Chapter7Problem6/src/chapter7problem6/Truck.java
@@ -1,14 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package chapter7problem6;
 
 /**
  *
- * @author torna
+ * @author Michael Sloan
  */
-public class Truck
+public class Truck extends Vehicle
 {
+  private double loadCapacity;
+  private int towingCapacity;
+  
+    
+  public Truck(String manufacturerName, int cylinders, double loadCapacity, int towingCapacity)
+  {
+    super(manufacturerName,cylinders);
+    this.loadCapacity = loadCapacity;
+    this.towingCapacity = towingCapacity;
+  }
   
 }
