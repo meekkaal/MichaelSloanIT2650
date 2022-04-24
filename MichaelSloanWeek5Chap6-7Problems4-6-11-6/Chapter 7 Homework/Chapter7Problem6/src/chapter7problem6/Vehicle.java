f6e2371138de97e969549b4103724471b0872561
Michael Sloan <mikesloan47@gmail.com>
2022-04-23 20:12:29 -0400

    Update Vehicle.java

--- MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 7 Homework/Chapter7Problem6/src/chapter7problem6/Vehicle.java
+++ MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 7 Homework/Chapter7Problem6/src/chapter7problem6/Vehicle.java
@@ -1,14 +1,72 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package chapter7problem6;
 
 /**
  *
- * @author torna
+ * @author Michael Sloan
  */
 public class Vehicle
 {
-  
+
+  private String manufacturerName;
+  private int cylinders;
+  private Person owner;
+
+  public Vehicle()
+  {
+
+  }
+
+  public Vehicle(String manufacturerName, int cylinders, Person owner)
+  {
+    this.manufacturerName = manufacturerName;
+    this.cylinders = cylinders;
+    this.owner = owner;
+  }
+
+  public Vehicle(String manufacturerName, int cylinders)
+  {
+    this.manufacturerName = manufacturerName;
+    this.cylinders = cylinders;
+  }
+
+  public String getManufacturerName()
+  {
+    return manufacturerName;
+  }
+
+  public void setManufacturerName(String manufacturerName)
+  {
+    this.manufacturerName = manufacturerName;
+  }
+
+  public int getCylinders()
+  {
+    return cylinders;
+  }
+
+  public void setCylinders(int cylinders)
+  {
+    this.cylinders = cylinders;
+  }
+
+  public Person getOwner()
+  {
+    return owner;
+  }
+
+  public void setOwner(Person owner)
+  {
+    this.owner = owner;
+  }
+
+  public String toString()
+  {
+    return "";
+  }
+
+  public boolean equals(Vehicle other)
+  {
+    return true;
+  }
+
 }
