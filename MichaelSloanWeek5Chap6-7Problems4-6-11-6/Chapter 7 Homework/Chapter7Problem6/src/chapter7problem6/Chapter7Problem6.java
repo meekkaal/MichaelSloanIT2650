c92e339e34f7a6d8d771f73c41675f2d40e18a41
Michael Sloan <mikesloan47@gmail.com>
2022-04-24 03:42:26 -0400

    Update Chapter7Problem6.java

--- MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 7 Homework/Chapter7Problem6/src/chapter7problem6/Chapter7Problem6.java
+++ MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 7 Homework/Chapter7Problem6/src/chapter7problem6/Chapter7Problem6.java
@@ -36,6 +36,47 @@ public class Chapter7Problem6
     Person p2 = new Person("Joe");
     System.out.println("p2: " + p2);
     
+    // test person 3 equals person 1
+    Person p3 = new Person("Bob");
+    System.out.println("p3 equal to p1: " + p3.equals(p1));
+    System.out.println("p2 equal to p1:  " + p2.equals(p1));
+    
+    // test vehicle 1
+    Vehicle v1 = new Vehicle();
+    System.out.println("v1: " + v1);
+    v1.setManufacturerName("Ford");
+    System.out.println("v1's manufacturer is: " + v1.getManufacturerName());
+    v1.setCylinders(4);
+    System.out.println("v1's cylinders: " + v1.getCylinders());
+    v1.setOwner(p2);
+    System.out.println("v1's owner is: " + v1.getOwner());
+    
+    // test vehicle 2
+    Vehicle v2 = new Vehicle("Chevy", 4, new Person("Betty"));
+    System.out.println("v2: " + v2);
+    
+    //test vehicle 3
+    Vehicle v3 = new Vehicle(v1);
+    System.out.println("v3 equal to v1: " + v3.equals(v1));
+    System.out.println("v2 equal to v1: " + v2.equals(v1));
+    
+    //test truck 1
+    Truck t1 = new Truck();
+    System.out.println("t1: " + t1);
+    t1.setLoadCapacity(54.36);
+    System.out.println("t1's load capacity is: " + t1.getLoadCapacity());
+    t1.setTowingCapacity(10);
+    System.out.println("t1's towing capacity is: " + t1.getTowingCapacity());
+    
+    //test truck 2
+    Truck t2 = new Truck("Chevy", 4, v2.getOwner(), 34.5, 65);
+    System.out.println("t2: " + t2);
+    
+    Truck t3 = new Truck(t1);
+    System.out.println("t3 equal to t1: " + t3.equals(t1));
+    System.out.println("v2 equal to t1: " + v2.equals(t1));
+    
+    
     
     
   }
