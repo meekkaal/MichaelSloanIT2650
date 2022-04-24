f371fd30fa4ffd8b3583f6cbd60659f0de320aa4
Michael Sloan <mikesloan47@gmail.com>
2022-04-24 03:42:32 -0400

    Update Truck.java

--- MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 7 Homework/Chapter7Problem6/src/chapter7problem6/Truck.java
+++ MichaelSloanWeek5Chap6-7Problems4-6-11-6/Chapter 7 Homework/Chapter7Problem6/src/chapter7problem6/Truck.java
@@ -9,12 +9,83 @@ public class Truck extends Vehicle
   private double loadCapacity;
   private int towingCapacity;
   
-    
-  public Truck(String manufacturerName, int cylinders, double loadCapacity, int towingCapacity)
+  public Truck()
   {
-    super(manufacturerName,cylinders);
+    loadCapacity = 0;
+    towingCapacity = 0; 
+  }  
+    
+  /**
+   *
+   * @param manufacturerName
+   * @param cylinders
+   * @param owner
+   * @param loadCapacity
+   * @param towingCapacity
+   */
+  public Truck(String manufacturerName, int cylinders, Person owner, double loadCapacity, int towingCapacity)
+  {
+    super(manufacturerName,cylinders, owner);
     this.loadCapacity = loadCapacity;
     this.towingCapacity = towingCapacity;
   }
   
+  public Truck(Truck truck)
+  {
+    super(truck);
+    loadCapacity = truck.loadCapacity;
+    towingCapacity = truck.towingCapacity;
+  }
+  
+  public double getLoadCapacity()
+  {
+    return loadCapacity;
+  }
+  
+  public void setLoadCapacity(double loadCapacity)
+  {
+    this.loadCapacity = loadCapacity;
+  }
+  
+  public int getTowingCapacity()
+  {
+    return towingCapacity;
+  }
+  
+  public void setTowingCapacity(int towingCapacity)
+  {
+    this.towingCapacity = towingCapacity;
+  }
+  
+  
+  @Override
+  public String toString()
+  {
+    return super.toString() + ", load capacity " + loadCapacity + ", towing capacity " + towingCapacity;
+  }
+  
+  /**
+   *
+   * @param other
+   * @return
+   */
+  @Override
+  public boolean equals(Object other)
+  {
+    if (other == null)
+    {
+      return false;
+    }
+    else if (getClass() != other.getClass())
+    {
+      return false;
+    }
+    else
+    {
+      Truck otherTruck = (Truck)other;
+      return (loadCapacity == otherTruck.loadCapacity 
+              && towingCapacity == otherTruck.towingCapacity 
+              && getOwner().equals(otherTruck.getOwner()));
+    }
+  }
 }
