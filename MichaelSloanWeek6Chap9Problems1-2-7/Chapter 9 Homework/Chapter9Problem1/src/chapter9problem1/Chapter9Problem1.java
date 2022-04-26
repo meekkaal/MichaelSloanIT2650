99011b36b9edda48d10ce358010c2c848d00c449
Michael Sloan <mikesloan47@gmail.com>
2022-04-26 01:55:02 -0400

    finish implementation

--- MichaelSloanWeek6Chap9Problems1-2-7/Chapter 9 Homework/Chapter9Problem1/src/chapter9problem1/Chapter9Problem1.java
+++ MichaelSloanWeek6Chap9Problems1-2-7/Chapter 9 Homework/Chapter9Problem1/src/chapter9problem1/Chapter9Problem1.java
@@ -7,6 +7,8 @@
  */
 package chapter9problem1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Michael Sloan
@@ -24,6 +26,43 @@ public class Chapter9Problem1
     System.out.println("Homework Assignment: Chapter 9, Problem 1");
     System.out.println("_________________________________");
     System.out.println("");
+
+    Scanner keyboard = new Scanner(System.in);
+    boolean done = false;
+
+    while (!done)
+    {
+      try
+      {
+        System.out.println("How many numbers do you want to enter?");
+        int[] numbers = new int[keyboard.nextInt()];
+        double sum = 0;
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+          System.out.println("Enter number " + (i + 1));
+          numbers[i] = keyboard.nextInt();
+          sum += numbers[i];
+        }
+
+        double average = sum / numbers.length;
+
+        System.out.println("The average is " + average);
+        System.out.println("Would you like to try again? (enter Q to quit, any other key to restart.)");
+        if (keyboard.next().toUpperCase().equals("Q"))
+        {
+          done = true;
+        }
+      }
+      catch (NegativeArraySizeException e)
+      {
+        System.out.println("Number must be greater than 0.");
+      }
+
+    }
+    
+    System.exit(0);
+
   }
-  
+
 }
