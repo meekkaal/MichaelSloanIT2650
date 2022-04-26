63e67ce1e1aab3ae3cad9cb0d55147d529a3ef85
Michael Sloan <mikesloan47@gmail.com>
2022-04-26 01:23:36 -0400

    initial commit for chapter 9 problems

---
+++ MichaelSloanWeek6Chap9Problems1-2-7/Chapter 9 Homework/Chapter9Problem1/src/chapter9problem1/Chapter9Problem1.java
@@ -0,0 +1,29 @@
+/*
+ * IT-2650 Java Programming, Spring 2022
+ * Instructor: Martin P. Walsh
+ * Student Name: Michael Sloan
+ * Homework Assignment: Chap 9 - Problem 1
+ * Purpose of Assignment:
+ */
+package chapter9problem1;
+
+/**
+ *
+ * @author Michael Sloan
+ */
+public class Chapter9Problem1
+{
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    System.out.println("IT-2650 Java Programming");
+    System.out.println("Student Name: Michael Sloan");
+    System.out.println("Homework Assignment: Chapter 9, Problem 1");
+    System.out.println("_________________________________");
+    System.out.println("");
+  }
+  
+}
---
+++ MichaelSloanWeek6Chap9Problems1-2-7/Chapter 9 Homework/Chapter9Problem7/src/chapter9problem7/Chapter9Problem7.java
@@ -0,0 +1,29 @@
+/*
+ * IT-2650 Java Programming, Spring 2022
+ * Instructor: Martin P. Walsh
+ * Student Name: Michael Sloan
+ * Homework Assignment: Chap 9 - Problem 7
+ * Purpose of Assignment:
+ */
+package chapter9problem7;
+
+/**
+ *
+ * @author Michael Sloan
+ */
+public class Chapter9Problem7
+{
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    System.out.println("IT-2650 Java Programming");
+    System.out.println("Student Name: Michael Sloan");
+    System.out.println("Homework Assignment: Chapter 9, Problem 7");
+    System.out.println("_________________________________");
+    System.out.println("");
+  }
+  
+}
