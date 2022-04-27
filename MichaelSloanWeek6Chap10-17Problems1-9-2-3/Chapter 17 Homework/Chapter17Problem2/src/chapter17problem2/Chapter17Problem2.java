84371bd5e817990d46123285900769e8494e174a
Michael Sloan <mikesloan47@gmail.com>
2022-04-27 02:05:16 -0400

    Update Chapter17Problem2.java

--- MichaelSloanWeek6Chap10-17Problems1-9-2-3/Chapter 17 Homework/Chapter17Problem2/src/chapter17problem2/Chapter17Problem2.java
+++ MichaelSloanWeek6Chap10-17Problems1-9-2-3/Chapter 17 Homework/Chapter17Problem2/src/chapter17problem2/Chapter17Problem2.java
@@ -7,11 +7,25 @@
  */
 package chapter17problem2;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 /**
  *
  * @author Michael Sloan
  */
-public class Chapter17Problem2
+public class Chapter17Problem2 extends JFrame
 {
 
   /**
@@ -24,6 +38,51 @@ public class Chapter17Problem2
     System.out.println("Homework Assignment: Chapter 17, Problem 2");
     System.out.println("_________________________________");
     System.out.println("");
+    
+    Chapter17Problem2 gui = new Chapter17Problem2();
+    gui.setVisible(true);
   }
-  
+  public Chapter17Problem2()
+  {
+    setSize(500,300);
+    setLayout(new BorderLayout());
+    setTitle("Tic-Tac-Toe");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    JPanel topPanel = new JPanel();
+    topPanel.setBackground(Color.LIGHT_GRAY);
+    topPanel.setLayout(new BorderLayout());
+
+    add(topPanel, BorderLayout.NORTH);
+    
+    JLabel messages = new JLabel("X's Turn");
+    topPanel.add(messages, BorderLayout.WEST);
+    
+    JButton newGame = new JButton("New Game");
+    topPanel.add(newGame, BorderLayout.EAST);
+    
+    JPanel gamePanel = new JPanel();
+    gamePanel.setLayout(new GridLayout(3,3,5,5) );
+    gamePanel.setBackground(Color.BLACK);
+    add(gamePanel, BorderLayout.CENTER);
+    
+    JButton button0 = new JButton();
+    gamePanel.add(button0);
+    JButton button1 = new JButton();
+    gamePanel.add(button1);
+    JButton button2 = new JButton();
+    gamePanel.add(button2);
+    JButton button3 = new JButton();
+    gamePanel.add(button3);
+    JButton button4 = new JButton();
+    gamePanel.add(button4);
+    JButton button5 = new JButton();
+    gamePanel.add(button5);
+    JButton button6 = new JButton();
+    gamePanel.add(button6);
+    JButton button7 = new JButton();
+    gamePanel.add(button7);
+    JButton button8 = new JButton();
+    gamePanel.add(button8);
+  }
 }
