f7ef19e8a21bce05c161f6bc92e7c7f86ea0bc76
Michael Sloan <mikesloan47@gmail.com>
2022-05-03 19:10:46 -0400

    Create Chapter18Problem7.java

---
+++ MichaelSloanWeek7Chap18Problem7/Chapter 18 Homework/Chapter18Problem7/src/chapter18problem7/Chapter18Problem7.java
@@ -0,0 +1,31 @@
+/*
+ * IT-2650 Java Programming, Spring 2022
+ * Instructor: Martin P. Walsh
+ * Student Name: Michael Sloan
+ * Homework Assignment: Chap 18 - Problem 7
+ * Purpose of Assignment:
+ */
+package chapter18problem7;
+
+/**
+ *
+ * @author Michael Sloan
+ */
+public class Chapter18Problem7
+{
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    System.out.println("IT-2650 Java Programming");
+    System.out.println("Student Name: Michael Sloan");
+    System.out.println("Homework Assignment: Chapter 18, Problem 7");
+    System.out.println("_________________________________");
+    System.out.println("");
+    
+    
+  }
+  
+}
