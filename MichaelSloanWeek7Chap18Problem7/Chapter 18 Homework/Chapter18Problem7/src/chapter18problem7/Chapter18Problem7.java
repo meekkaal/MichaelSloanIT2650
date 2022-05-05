30e24f28bd7f8d0182db34815f2ed28a78c2f0a8
Michael Sloan <mikesloan47@gmail.com>
2022-05-05 01:35:33 -0400

    Update Chapter18Problem7.java

--- MichaelSloanWeek7Chap18Problem7/Chapter 18 Homework/Chapter18Problem7/src/chapter18problem7/Chapter18Problem7.java
+++ MichaelSloanWeek7Chap18Problem7/Chapter 18 Homework/Chapter18Problem7/src/chapter18problem7/Chapter18Problem7.java
@@ -7,12 +7,22 @@
  */
 package chapter18problem7;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Michael Sloan
  */
 public class Chapter18Problem7
 {
+  
 
   /**
    * @param args the command line arguments
@@ -24,9 +34,61 @@ public class Chapter18Problem7
     System.out.println("Homework Assignment: Chapter 18, Problem 7");
     System.out.println("_________________________________");
     System.out.println("");
-    
-    MouseDemoFrame gui = new MouseDemoFrame();
+
+    MouseDemo gui = new MouseDemo();
     gui.setVisible(true);
   }
+}
+
+class MouseDemo extends JFrame implements MouseListener
+{
+  private List<Point> points = new ArrayList<>();
+
+  public void mouseClicked(MouseEvent e)
+  {
+    points.add(e.getPoint());
+    System.out.println(e.getX() + " " + e.getY());
+    repaint();
+  }
+
+  @Override
+  public void mouseEntered(MouseEvent e)
+  {
+  }
+
+  @Override
+  public void mousePressed(MouseEvent e)
+  {
+  }
+
+  @Override
+  public void mouseReleased(MouseEvent e)
+  {
+  }
+
+  @Override
+  public void mouseExited(MouseEvent e)
+  {
+  }
   
+  @Override
+  public void paint(Graphics g)
+  {
+    super.paint(g);
+    g.setColor(Color.BLUE);
+    for (Point point : points)
+    {
+      // center the drawn circle over the mouse click
+      g.fillOval(point.x - 8, point.y - 8, 16, 16);
+    }
+  }
+
+  public MouseDemo()
+  {
+    super();
+    setSize(600, 400);
+    setTitle("Mouse Demo");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    addMouseListener(this);
+  }
 }
